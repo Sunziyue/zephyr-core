930c3e909da4554ed1f30a8c220acd894744cafd
Sunziyue <Sunziyue@live.com>
2021-06-12 23:29:07 +0800

    切换 admin zephyr、logback

--- core-admin-starter/src/main/java/xyz/szy/zephyr/core/admin/starter/CoreAdminStarterAutoConfiguration.java
+++ core-admin-starter/src/main/java/xyz/szy/zephyr/core/admin/starter/CoreAdminStarterAutoConfiguration.java
@@ -1,7 +1,7 @@
 package xyz.szy.zephyr.core.admin.starter;
 
+import xyz.sunziyue.boot.swagger.autoconfig.SwaggerAutoConfiguration;
 import xyz.szy.zephyr.core.admin.CoreAdminAutoConfiguration;
-import io.terminus.common.swagger.autoconfig.SwaggerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
--- core-admin/src/main/java/xyz/szy/zephyr/core/admin/CoreAdminAutoConfiguration.java
+++ core-admin/src/main/java/xyz/szy/zephyr/core/admin/CoreAdminAutoConfiguration.java
@@ -1,14 +1,14 @@
 package xyz.szy.zephyr.core.admin;
 
-import xyz.szy.zephyr.core.api.client.CoreApiAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import xyz.szy.zephyr.core.api.client.CoreApiAutoConfiguration;
 
 @Configuration
 @ComponentScan
 @Import({CoreApiAutoConfiguration.class})
-public class CoreAdminAutoConfiguration extends WebMvcConfigurerAdapter {
+public class CoreAdminAutoConfiguration implements WebMvcConfigurer {
 
 }
